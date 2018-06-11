5f8b666e95d1a9558e2608de66fa811f9b1a4f4b
Agustin <agustinrc101@gmail.com>
2018-06-10 21:50:24 -0700

    Began new assimp import

---
+++ PA4/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/agr005/Downloads/assimp-4.0.1/assimp-4.0.1/contrib/zlib
+# Build directory: D:/gussug/CSE190-FinalProject/PA4/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ PA4/assimp/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/agr005/Downloads/assimp-4.0.1/assimp-4.0.1/test
+# Build directory: D:/gussug/CSE190-FinalProject/PA4/assimp/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittests "unit")
+subdirs("headercheck")
